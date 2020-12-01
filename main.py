1488f4fe93a758a9e9bafb134307acc9b0dab6da
furkankamer <furkankamer@gmail.com>
2020-12-01 23:55:10 +0300

    https routing

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ rooms = {}
 
 @app.before_request
 def before_request():
-    if not request.is_secure():
+    if not request.is_secure:
         url = request.url.replace('http://', 'https://', 1)
         code = 301
         return redirect(url, code=code)
